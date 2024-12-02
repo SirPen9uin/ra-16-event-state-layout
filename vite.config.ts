f55c72c83dccc4da2f8bd6404f538420e625a307
Pen9uin <sirpen9uin@gmail.com>
2024-12-02 23:41:40 +0300

    another fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/layout/',
+  base: '/ra-16-event-state-layout/',
   plugins: [react()],
 })
